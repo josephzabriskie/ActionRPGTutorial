af9a14d9d7968fd10e2cb65f4ad18137d7a45ede
Joe Zabriskie <josephzabriskie@gmail.com>
2017-11-14 23:04:09 -0700

    Added basic tiles and character movement, Episode 2 Done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizInput = Input.GetAxisRaw ("Horizontal");
+		if(horizInput > 0.5f || horizInput < -0.5f){
+			transform.Translate (new Vector3 (horizInput * moveSpeed * Time.deltaTime, 0f, 0f));
+		}
+		float vertInput = Input.GetAxisRaw ("Vertical");
+		if(vertInput > 0.5f || vertInput < -0.5f){
+			transform.Translate (new Vector3 (0f, vertInput * moveSpeed * Time.deltaTime, 0f));
+		}
+	}
+}
