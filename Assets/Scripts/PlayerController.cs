edd2ef9b447e8f4578e17312756303d792747d6d
Joe Zabriskie <josephzabriskie@gmail.com>
2017-11-16 23:39:23 -0700

    Switched animations to blend tree. Episode 4 Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour {
 
 	private Animator anim;
 
+	private bool playerMoving;
+	private Vector2 lastMove;
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
@@ -14,16 +17,25 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		playerMoving = false;
 		float horizInput = Input.GetAxisRaw ("Horizontal");
+		float vertInput = Input.GetAxisRaw ("Vertical");
+
 		if(horizInput > 0.5f || horizInput < -0.5f){
 			transform.Translate (new Vector3 (horizInput * moveSpeed * Time.deltaTime, 0f, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (horizInput, 0f);
 		}
-		float vertInput = Input.GetAxisRaw ("Vertical");
 		if(vertInput > 0.5f || vertInput < -0.5f){
 			transform.Translate (new Vector3 (0f, vertInput * moveSpeed * Time.deltaTime, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (0f, vertInput);
 		}
 
 		anim.SetFloat ("MoveX", horizInput);
 		anim.SetFloat ("MoveY", vertInput);
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat("LastMoveY", lastMove.y);
 	}
 }
